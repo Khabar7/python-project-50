a88e2f2e301555ab85b4b96656640b0d59f474e8
khabar7 <khabarov.ilya7@yandex.ru>
2022-11-25 17:45:52 +0400

    update gendiff.py step2

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -5,5 +5,6 @@ parser.add_argument('strings', metavar='first_file', type=str, nargs=1,
                     help='')
 parser.add_argument('strings', metavar='second_file', type=str, nargs=1,
                     help='')
+parser.add_argument('-f', '--format', help='set format of output')
 
 args = parser.parse_args()
